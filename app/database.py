10fecfdadf1f9bf4f1e33caf836018fb8f9e321b
fash-io <favourfasi46@gmail.com>
2025-05-05 13:05:59 +0100

    fuck

--- app/database.py
+++ app/database.py
@@ -1,57 +1,28 @@
-"""
-database.py
-
-Sets up SQLAlchemy engine and session for connecting to Supabase Postgres via DATABASE_URL.
-"""
-import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+# from sqlalchemy.pool import NullPool
 from dotenv import load_dotenv
+import os
 
 # Load environment variables from .env
 load_dotenv()
 
-# Read the DATABASE_URL environment variable
 # Fetch variables
-USER = os.environ.get("user")
-PASSWORD = os.environ.get("password")
-HOST = os.environ.get("host")
-PORT = os.environ.get("port")
-DBNAME = os.environ.get("dbname")
-# Create SQLAlchemy engine
+USER = os.getenv("user")
+PASSWORD = os.getenv("password")
+HOST = os.getenv("host")
+PORT = os.getenv("port")
+DBNAME = os.getenv("dbname")
 
+# Construct the SQLAlchemy connection string
 DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
 
-# echo=True will log all the SQL queries; turn off in production
-engine = create_engine(
-    DATABASE_URL,
-    echo=True,
-    future=True  # use SQLAlchemy 2.0 style
-)
+# Create the SQLAlchemy engine
+engine = create_engine(DATABASE_URL)
+# If using Transaction Pooler or Session Pooler, we want to ensure we disable SQLAlchemy client side pooling -
+# https://docs.sqlalchemy.org/en/20/core/pooling.html#switching-pool-implementations
+# engine = create_engine(DATABASE_URL, poolclass=NullPool)
 
-# Create a configured "Session" class
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine,
-    future=True
-)
-
-# Base class for ORM models
-Base = declarative_base()
-
-# Dependency to get DB session in FastAPI endpoints
-
-def get_db():
-    """
-    Yield a SQLAlchemy database session, closing it after use.
-    """
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
+# Test the connection
 try:
     with engine.connect() as connection:
         print("Connection successful!")
