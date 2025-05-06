bf19f61b15e10f64681c4bf1eb33479635250bdb
fash-io <favourfasi46@gmail.com>
2025-05-06 21:09:37 +0100

    Fuck my life

--- app/routes/votes.py
+++ app/routes/votes.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from uuid import UUID
-from typing import  Annotated
+from typing import Annotated
 
 from app.dependencies import get_db
 from app.database import get_db
